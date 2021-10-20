4e6b8d07f8f21157055fe33af6629c2b58a8b729
vrussillo <80196694+vrussillo@users.noreply.github.com>
2021-10-19 20:05:59 -0700

    broken solution

---
+++ models.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+"""Models for Cupcake app."""
+
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    """cupcakes."""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    flavor = db.Column(db.Text, nullable=False)
+
+    size = db.Column(db.Text, nullable=False)
+
+    rating = db.Column(db.Float, nullable=False)
+
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def to_dict(self):
+        """Serialize cupcake to a dict of cupcake info."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "rating": self.rating,
+            "size": self.size,
+            "image": self.image,
+        }
+
+
